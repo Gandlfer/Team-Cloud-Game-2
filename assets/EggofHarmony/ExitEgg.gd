4e2a327d8eca920ba926559f47ba2b43d5bbaad4
AsianCawFee <darryllee1999@gmail.com>
2024-03-13 15:09:54 -0500

    Added egg of harmony placeholder for detecting player collision to move next level

---
+++ assets/EggofHarmony/ExitEgg.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.name == "CharacterBody2D":
+		# unlock powers
+		print("Next Scene")
+		$Area2D.queue_free()
+		$Sprite2D.queue_free()

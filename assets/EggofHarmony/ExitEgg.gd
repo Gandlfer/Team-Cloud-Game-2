61bedcc6826ef009c001eb0639062bdb857e0650
AsianCawFee <darryllee1999@gmail.com>
2024-03-16 16:36:12 -0500

    move to next scene

--- assets/EggofHarmony/ExitEgg.gd
+++ assets/EggofHarmony/ExitEgg.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-
+@export var target_level : PackedScene
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -18,3 +18,4 @@ func _on_area_2d_body_entered(body):
 		print("Next Scene")
 		$Area2D.queue_free()
 		$AnimatedSprite2D.queue_free()
+		get_tree().change_scene_to_packed(target_level)

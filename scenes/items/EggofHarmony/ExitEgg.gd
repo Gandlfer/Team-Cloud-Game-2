0a964c456d1a53f6a3af1b964eed4540f39f70f7
AsianCawFee <darryllee1999@gmail.com>
2024-03-17 14:08:35 -0500

    Fix egg

--- scenes/items/EggofHarmony/ExitEgg.gd
+++ scenes/items/EggofHarmony/ExitEgg.gd
@@ -16,13 +16,13 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	if body.name == "CharacterBody2D":
+	
+	if body.is_in_group("Player"): 
 		# unlock powers
 		print("Next Scene")
 		$Area2D.queue_free()
 		$AnimatedSprite2D.queue_free()
 		$"../CharacterBody2D".set_physics_process(false)
-		#$"../CanvasLayer".visible=true
 		$"../Transition".transitionToBlack()
 		await get_tree().create_timer(0.5).timeout
 		get_tree().change_scene_to_packed(target_level)

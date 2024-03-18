e461583530f3c6d97f5fa9b12ba0d802b49909e9
chloepiper <chloepiper1207@gmail.com>
2024-03-18 04:09:55 -0500

    add gunshot sound effect, bugfixes

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -43,7 +43,7 @@ func _ready():
 		#print("true")
 		dash = false
 		gun = false
-	elif get_parent().name == "DashLevel":
+	elif get_parent().name == "Node":
 		dash = true
 		gun = false
 	elif get_parent().name == "BossLevel":
@@ -60,7 +60,9 @@ func shoot(isLeft):
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("shoot"):
+		$SoundShoot.play()
 		shoot(isLeft)
+		
 	# animations
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "run_puffel"
@@ -83,6 +85,7 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 		$SoundJump.play()
 	elif Input.is_action_just_pressed("jump") and !is_on_floor() and canDoubleJump:
+		$SoundJump.play()
 		canDoubleJump = false
 		velocity.y = JUMP_VELOCITY
 	
